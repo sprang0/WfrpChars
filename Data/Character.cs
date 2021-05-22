86312f1f471a7bdcd06ba5735e8d43aff9c5636e
Todd Sprang <sprang@gmail.com>
2021-05-21 22:51:25 -0400

    character class

---
+++ Data/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace WfrpChars.Data
+{
+    class Character
+    {
+        public Character(ISpecies species)
+        {
+            Species = species;
+        }
+
+        public ISpecies Species { get; set; } = new Human();
+        public Characteristics Characteristics { get; set; } = new Characteristics();
+    }
+}
--- Data/Characteristics.cs
+++ Data/Characteristics.cs
@@ -7,5 +7,12 @@ namespace WfrpChars.Data
         public Characteristic BallisticSkill { get; set; }
         public Characteristic Strength { get; set; }
         public Characteristic Toughness { get; set; }
+        public Characteristic Initiative { get; set; }
+        public Characteristic Agility { get; set; }
+        public Characteristic Dexterity { get; set; }
+        public Characteristic Intelligence { get; set; }
+        public Characteristic Willpower { get; set; }
+        public Characteristic Fellowship { get; set; }
+        public Characteristic Wounds { get; set; }
     }
 }
